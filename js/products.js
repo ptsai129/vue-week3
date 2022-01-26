cbd0d92b060819911d723d76f08b84441615a6d7
“PeggyTsai” <pinklover1671291991@gmail.com>
2022-01-26 22:20:04 +0800

    新增/編輯/刪除方法

--- js/products.js
+++ js/products.js
@@ -1,3 +1,6 @@
+let productModal = null;
+let delProductModal = null;
+
 Vue.createApp({
     data(){
         return{
@@ -5,7 +8,9 @@ Vue.createApp({
             apiPath : 'ptsai129', //個人 API Path
             //放產品列表資料
             products :[],
-            //放產品明細資料
+            //判斷是新增或編輯產品
+            AddNewProduct:false, 
+            //放modal明細資料?
             temp:{}
         }
     }, 
@@ -18,7 +23,7 @@ Vue.createApp({
            this.getProductList(); 
           
         }).catch((error)=>{
-            alert(error);
+            alert(error.data.message);
           //跳轉到登入頁面
           window.location = 'index.html';
         })
@@ -32,6 +37,30 @@ Vue.createApp({
                 console.log(error);
             })
 
+        },
+        //依據帶入的參數判別出modal顯示的內容 
+        openModal( editState , item){
+            if (editState === 'new'){
+                 this.temp = {
+                     imagesUrl :[],
+                 };
+                //modal標題顯示新增產品
+                this.AddNewProduct = true;
+                //顯示modal
+                productModal.show();
+            }else if ( editState === 'edit'){
+                //複製products內的item內容到temp內
+                this.temp = {...item};
+                //modal標題顯示編輯產品
+                this.AddNewProduct = false; 
+                productModal.show();
+            }else if (editState ==='delete'){
+                 //複製products內的item內容到temp內
+                this.temp ={...item};
+                //顯示modal
+                delProductModal.show()
+            }
+            
         }
 
     }, 
@@ -40,7 +69,20 @@ Vue.createApp({
           const token = document.cookie.replace(/(?:(?:^|.*;\s*)myToken\s*\=\s*([^;]*).*$)|^.*$/, "$1");
           axios.defaults.headers.common['Authorization'] = token;
           this.checkSignIn();
+         
+
+         //用一行 JavaScript 創建一個互動視窗
+         // 使用 new 建立 bootstrap Modal，拿到實體 DOM 並賦予到變數上
+          productModal = new bootstrap.Modal(document.getElementById('productModal'), {
+            keyboard: false //取消使用esc關閉modal功能
+        });
+        delProductModal = new bootstrap.Modal(document.getElementById('delProductModal'), {
+            keyboard: false//取消使用esc關閉modal功能
+        });
 
     }
 
 }).mount("#app");
+
+
+
